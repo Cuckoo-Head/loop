c08ccfde52af0675d311920fdfca582bce090b94
Cuckoo-Head <cuckoohead@disroot.org>
2022-01-14 12:55:31 -0300

    exercicio 1 resolvido

--- src/com/dio/exercicios/loop/Ex1.java
+++ src/com/dio/exercicios/loop/Ex1.java
@@ -1,10 +1,27 @@
 package com.dio.exercicios.loop;
 
+import java.util.Scanner;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
 
+        String nome;
+        int idade;
+
+        while(true) {
+            System.out.println("Nome: ");
+            nome = scan.next(); // nextLine() não funciona
+            if (nome.equals("0")) break;
+
+            System.out.println("Idade: ");
+            idade = scan.nextInt();
+        }
+
+        System.out.println("Obrigado.");
+        
 	}
 
 }
