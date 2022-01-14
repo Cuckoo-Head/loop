da3943f5926e18755f70b90950983d7de23e4640
Cuckoo-Head <cuckoohead@disroot.org>
2022-01-14 15:15:18 -0300

    exercicio 2 resolvido

--- src/com/dio/exercicios/loop/Ex2.java
+++ src/com/dio/exercicios/loop/Ex2.java
@@ -1,10 +1,23 @@
 package com.dio.exercicios.loop;
 
+import java.util.Scanner;
+
 public class Ex2 {
 
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
 		
+		int nota;
+		String mensagem;
+		
+		System.out.println("Digite a nota: ");
+		nota = scan.nextInt();
+		
+		while(nota < 0 | nota > 10) {
+			System.out.println("Nota inválida, digite novamente:");
+			nota = scan.nextInt();
+		}
 	}
 
 }
