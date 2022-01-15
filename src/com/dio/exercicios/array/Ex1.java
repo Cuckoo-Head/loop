c264c47bc6b20be3eb78b35b5f463e28a1c5a2ba
Cuckoo-Head <cuckoohead@disroot.org>
2022-01-14 21:15:44 -0300

    exercicio array.Ex1 resolvido

--- src/com/dio/exercicios/array/Ex1.java
+++ src/com/dio/exercicios/array/Ex1.java
@@ -4,6 +4,11 @@ public class Ex1 {
 
 	public static void main(String[] args) {
 
+		int arrayNum[] = {-2,3,9,-1,0,7};
+		
+		for(int i = (arrayNum.length -1); i > 0; i--) {
+			System.out.println(arrayNum[i]);
+		}
 		
 	}
 
