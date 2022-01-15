f52eb9bf8e121963977b5cb3c1c452abe974fa86
Cuckoo-Head <cuckoohead@disroot.org>
2022-01-14 23:10:18 -0300

    exercicio Ex4 resolvido

--- src/com/dio/exercicios/array/Ex4.java
+++ src/com/dio/exercicios/array/Ex4.java
@@ -1,10 +1,26 @@
 package com.dio.exercicios.array;
 
+import java.util.Random;
+
 public class Ex4 {
 
 	public static void main(String[] args) {
 
+		Random random = new Random();
+		int[][] matrix = new int[4][4];
 		
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[i].length; j++) {
+				matrix[i][j] = random.nextInt(9);
+			}
+		}
+		
+		for(int[] linha : matrix) {
+			for(int coluna : linha) {
+				System.out.print(coluna + " ");
+			}
+			System.out.println();
+		}
 	}
 
 }
